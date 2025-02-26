818d1fc350171fb9a785b9ba3fa6405f3856fd5b
Onkar <Omkar60403059@gmail.com>
2025-02-26 17:43:02 +0530

    fixed dashboard responsiveness bug

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -223,10 +223,10 @@ function Dashboard() {
       </div>
 
       {/* KPI Cards Grid */}
-      <div className="flex overflow-x-auto mx-auto gap-6 mb-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
         {/* Total Appointments */}
-        <div className="bg-white p-4 rounded-lg  border border-gray-200 lg:px-[70px]">
-          <div className="flex items-center text-left gap-2 mb-2 ">
+        <div className="bg-white p-4 rounded-lg  border border-gray-200">
+          <div className="flex items-center gap-2 mb-2">
             <span className="text-yellow-500 text-lg"><HiUsers /></span>
             <h3 className="text-sm sm:text-base font-medium text-gray-600">Total Appointments</h3>
           </div>
@@ -235,8 +235,8 @@ function Dashboard() {
         </div>
 
         {/* Pending */}
-        <div className="bg-white p-4 rounded-lg  border border-gray-200 px-[70px]">
-          <div className="flex items-start gap-2 mb-2">
+        <div className="bg-white p-4 rounded-lg  border border-gray-200">
+          <div className="flex items-center gap-2 mb-2">
             <span className="text-purple-500 text-lg"><SlCalender /></span>
             <h3 className="text-sm sm:text-base font-medium text-gray-600">Pending</h3>
           </div>
@@ -245,17 +245,17 @@ function Dashboard() {
         </div>
 
         {/* Total Earnings */}
-        <div className="bg-white p-4 rounded-lg  border border-gray-200 px-[70px]">
+        <div className="bg-white p-4 rounded-lg  border border-gray-200">
           <div className="flex items-center gap-2 mb-2">
             <span className="text-yellow-500 text-lg"><FaIndianRupeeSign /></span>
             <h3 className="text-sm sm:text-base font-medium text-gray-600">Total Earnings</h3>
           </div>
-          <p className="text-2xl font-bold text-gray-900"> 75275</p>
+          <p className="text-2xl font-bold text-gray-900">₹ 75275</p>
           <p className="text-xs sm:text-sm text-gray-500">last 7 days <span className="text-green-500">300.00% ↑</span></p>
         </div>
 
         {/* Patient Queue */}
-        <div className="bg-white p-4 rounded-lg  border border-gray-200 px-[70px]">
+        <div className="bg-white p-4 rounded-lg  border border-gray-200">
           <div className="flex items-center gap-2 mb-2">
             <span className="text-blue-500 text-lg"><FaUsers /></span>
             <h3 className="text-sm sm:text-base font-medium text-gray-600">Patient Queue</h3>
